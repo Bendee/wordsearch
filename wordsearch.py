86321097f4ee40ae08fad5ba5185a64e165585b3
Benjamin Doerry <ben.doerry@gmail.com>
2020-10-29 18:07:31 +0000

    Read words file from stdin

--- wordsearch.py
+++ wordsearch.py
@@ -1,18 +1,34 @@
 #! /usr/bin/env python3
+from sys import stdin
+from typing import Iterable, List
+
+
+ROW_LENGTH: int = 10000
+CHARS: str = 'abcdefghijklmnopqrstuvwxyz'
 
 
 class WordSearch(object):
 
-    def __init__(self, grid):
+    def __init__(self, grid: str) -> None:
         pass
 
-    def is_present(self, word):
+    def is_present(self, word: str) -> bool:
         return True
 
 
-if __name__ == "__main__":
-    ws = WordSearch(grid)
+def read_grid() -> str:
+    words: Iterable[str] = map(lambda x: x.lower(), stdin.readlines())
+    filtered_words: List[str] = [
+        ''.join(filter(lambda x: x in CHARS, word))
+        for word in words
+    ]
+    return ''.join(filtered_words)
 
-    for word in words_to_find:
-        if ws.is_present(word):
-            print "found {}".format(word)
+
+if __name__ == "__main__":
+    grid: str = read_grid()
+    ws: WordSearch = WordSearch(grid)
+
+    # for word in words_to_find:
+    #     if ws.is_present(word):
+    #         print "found {}".format(word)
