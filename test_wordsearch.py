af5519da063eea149acff0b5be9ba1f91798f77e
Benjamin Doerry <ben.doerry@gmail.com>
2020-11-06 11:30:20 +0000

    Benchmark word presence

--- test_wordsearch.py
+++ test_wordsearch.py
@@ -1,5 +1,8 @@
-from random import choice
+from random import choice, randrange, sample
 from string import ascii_lowercase
+from typing import Dict
+
+from pytest import mark
 
 from wordsearch import WordSearch, read_grid
 
@@ -18,11 +21,27 @@ def create_test_grid() -> None:
             ) + '\n')
 
 
+def get_words(amount: int = 100) -> Dict[str, bool]:
+    words: Dict[str, bool] = {}
+    for i in range(amount):
+        axis: str = choice(choice((WS.rows, WS.columns)))
+        index: int = randrange(AXIS_LENGTH)
+
+        in_word: str = axis[index:index + randrange(5, max(12, min(24, (AXIS_LENGTH - index))))]
+        words[in_word] = True
+
+        out_word: str = ''.join(sample(in_word, len(in_word)))
+        words[out_word] = WS.is_present(out_word)
+
+    return words
+
+
 if NEW_GRID:
     create_test_grid()
 
 GRID: str = read_grid(GRID_FILE)
 WS: WordSearch = WordSearch(GRID, AXIS_LENGTH)
+WORDS: Dict[str, bool] = get_words()
 
 
 def test_read_grid(benchmark):
@@ -35,3 +54,15 @@ def test__generate_rows(benchmark):
 
 def test__generate_columns(benchmark):
     benchmark(WS._generate_columns)
+
+
+@mark.parametrize("word, expected", WORDS.items())
+def test_is_present(benchmark, word, expected):
+    result = benchmark(WS.is_present, word=word)
+    assert result == expected
+
+
+@mark.parametrize("word, expected", WORDS.items())
+def test__is_present(benchmark, word, expected):
+    result = benchmark(WS._is_present, word=word)
+    assert result == expected
