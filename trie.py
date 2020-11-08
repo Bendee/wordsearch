f0b12e8d1a6c33981c8bc2a5b1494344b7df1d29
Benjamin Doerry <ben.doerry@gmail.com>
2020-11-08 21:40:29 +0000

    Implement construction of Trie

--- trie.py
+++ trie.py
@@ -9,6 +9,7 @@ if TYPE_CHECKING:
 
 GRID = 'bgvtt zpibu vxzft oakis fvqwl'
 AXIS_LENGTH = 5  # type: int
+MAX_WORD_LENGTH = 24  # type: int
 
 
 class TrieNode:
@@ -17,9 +18,44 @@ class TrieNode:
         self.character = character  # type: str
         self._children = {}  # type: ChildMap
 
+    def append(self, character: str, children: 'List[str]') -> None:
+        node = self._children.get(character) or TrieNode(character)
+
+        if children.size != 0:
+            node.append(children[:1][0], children[1:])
+
+        self._children[character] = node
+
+
+class Trie:
+
+    def __init__(self, grid: 'List[List[str]]') -> None:
+        self.root = TrieNode('')
+        self.grid = grid
+
+        for row in self.grid:
+            self.iterate_axis(row)
+        for column in self.grid.T:
+            self.iterate_axis(column)
+
+    def iterate_axis(self, axis: 'List[str]'):
+        for index, character in enumerate(axis):
+            self.root.append(
+                character,
+                axis[index+1:min(AXIS_LENGTH, index + MAX_WORD_LENGTH)],
+            )
+
+
+def read_grid() -> str:
+    return GRID.replace(' ', '')
+
 
 def format_grid(grid: str) -> 'List[List[str]]':
     return reshape(
         fromstring(grid, dtype='S1', count=AXIS_LENGTH**2),
         [AXIS_LENGTH]*2,
     )
+
+
+if __name__ == '__main__':
+    trie = Trie(format_grid(read_grid()))  # type: Trie
