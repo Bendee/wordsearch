f1b0cd8eafc905638729ffdb54c66c26cc9aabdd
Benjamin Doerry <ben.doerry@gmail.com>
2020-11-08 21:37:21 +0000

    Implement TrieNode

--- trie.py
+++ trie.py
@@ -2,10 +2,20 @@ from typing import TYPE_CHECKING
 from numpy import fromstring, reshape
 
 if TYPE_CHECKING:
+    from typing import Dict, List
+
+    ChildMap = Dict[str, 'TrieNode']
 
 
 GRID = 'bgvtt zpibu vxzft oakis fvqwl'
 AXIS_LENGTH = 5  # type: int
+
+
+class TrieNode:
+
+    def __init__(self, character: str) -> None:
+        self.character = character  # type: str
+        self._children = {}  # type: ChildMap
 
 
 def format_grid(grid: str) -> 'List[List[str]]':
