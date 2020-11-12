554363b1a9d1d6b1c304c7861b2cc9552b72913b
Benjamin Doerry <ben.doerry@gmail.com>
2020-11-12 21:35:08 +0000

    Add worker functions to grid tests

--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -4,33 +4,44 @@ from math import sqrt
 
 
 if TYPE_CHECKING:
-    from typing import Dict, Iterable
+    from typing import Dict, List
+    from typing_extensions import TypedDict
 
     WordMap = Dict[str, bool]
+    WordsMaps = TypedDict(
+        'WordsMaps',
+        {
+            'all': WordMap,
+            'index_100': WordMap,
+        }
+    )
 
 
-def _load_file(path: str) -> 'Iterable[str]':
+def _load_file(path: str) -> 'List[str]':
     with open(path, 'r') as file:
-        lines = iter(
+        lines = [
             line.strip()
             for line in file
-        )  # type: Iterable[str]
+        ]  # type: List[str]
 
     return lines
 
-def _load_json(path: str) -> 'WordMap':
+def _load_json(path: str) -> 'WordsMaps':
     with open(path, 'r') as file:
-        json = load(file)  # type: WordMap
+        json = load(file)  # type: WordsMaps
 
     return json
 
 
-GRID_FILE = 'test_grid.txt'  # type: str
-WORDS_FILE = 'test_words.txt'  # type: str
-_WORDS_JSON = 'test_words.json'  # type: str
+GRID_FILE = 'tests/data/test_grid.txt'  # type: str
+WORDS_FILE = 'tests/data/test_words.txt'  # type: str
+_WORDS_JSON = 'tests/data/test_words.json'  # type: str
 WINDOW_SIZE = 100  # type: int
 
 GRID = ''.join(_load_file(GRID_FILE))  # type: str
 ROW_LENGTH = int(sqrt(len(GRID)))  # type: int
-WORDS = _load_file(WORDS_FILE)  # type: Iterable[str]
-WORDS_MAP = _load_json(_WORDS_JSON)  # type: WordMap
+WORDS = _load_file(WORDS_FILE)  # type: List[str]
+
+_WORDS_MAPS = _load_json(_WORDS_JSON)  # type: WordsMaps
+WORDS_MAP = _WORDS_MAPS['all']  # type: WordMap
+WINDOW_WORDS = _WORDS_MAPS['index_100']  # type: WordMap
--- tests/utils/test_grid.py
+++ tests/utils/test_grid.py
@@ -2,16 +2,32 @@ from typing import TYPE_CHECKING
 
 from pytest import mark
 
-from utils.grid import Grid
+from utils.grid import Grid, _share_axes, _contains_word
 
-from tests.data import GRID, ROW_LENGTH, WINDOW_SIZE, WORDS_MAP
+from tests.data import GRID, ROW_LENGTH, WINDOW_SIZE, WORDS_MAP, WINDOW_WORDS
 
 
 if TYPE_CHECKING:
-    from utils.grid import Axes
+    from utils.grid import Axes, AxesInfo
 
 
 GRID_INSTANCE = Grid(GRID, axis_length=ROW_LENGTH, window_size=WINDOW_SIZE)  # type: Grid
+AXES_INFO = {
+    'rows': GRID_INSTANCE._shared_rows,
+    'columns': GRID_INSTANCE._shared_columns,
+    'window': GRID_INSTANCE._window_size,
+}  # type: AxesInfo
+
+
+def test_share_axes(benchmark) -> None:
+    benchmark(_share_axes, axes_info=AXES_INFO)
+
+
+@mark.parametrize('word, expected', WINDOW_WORDS.items())
+def test_contains_word(benchmark, word: str, expected: bool) -> None:
+    _share_axes(AXES_INFO)
+    result = benchmark(_contains_word, word=word, search_index=100)  # type: bool
+    assert result == expected
 
 
 def test__generate_rows(benchmark) -> None:
@@ -22,12 +38,12 @@ def test__generate_columns(benchmark) -> None:
     benchmark(GRID_INSTANCE._generate_columns)
 
 
-@mark.parametrize("axes", (GRID_INSTANCE.rows, GRID_INSTANCE.columns))
+@mark.parametrize('axes', (GRID_INSTANCE.rows, GRID_INSTANCE.columns))
 def test__share_axes(benchmark, axes: 'Axes') -> None:
     benchmark(GRID_INSTANCE._share_axes, axes=axes)
 
 
-@mark.parametrize("word, expected", WORDS_MAP.items())
+@mark.parametrize('word, expected', WORDS_MAP.items())
 def test_linear_search(benchmark, word: str, expected: bool) -> None:
     result = benchmark(GRID_INSTANCE.linear_search, word=word)  # type: bool
     assert result == expected
